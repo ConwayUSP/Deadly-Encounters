5d914a9b69563daf7e94363e40778ae722d1f2d3
Caio Bernardo <me@caiobernardo.top>
2026-03-20 12:52:39 -0300

    feat: add transitions module and configure transition on the scene switching

--- main.lua
+++ main.lua
@@ -8,7 +8,9 @@ require("modules.gamestate")
 require("modules.oponent")
 require("modules.finalscreen")
 
+
 local Player = require("modules.player")
+local Transition = require("modules.engine.transition")
 
 GameCtx = CTX.MENU
 
@@ -19,26 +21,33 @@ GameState[CTX.SHOP] = GAMESTATE[CTX.SHOP]
 GameState[CTX.DEATH_SCREEN] = GAMESTATE[CTX.DEATH_SCREEN]
 GameState[CTX.VICTORY_SCREEN] = GAMESTATE[CTX.VICTORY_SCREEN]
 
+MainTransition = Transition.new(0.5, Transition.FADEINOUT)
+
 -- Função auxiliar para trocar de contexto e carregar o novo estado
--- TODO: inserir transição
 function SetGameCtx(newCtx)
-	GameCtx = newCtx
-	GameState[GameCtx]:load()
+    MainTransition:start(function()
+        GameCtx = newCtx
+        GameState[GameCtx]:load()
+    end)
 end
 
 function love.load()
 	love.window.setFullscreen(true)
 
-	-- carrega o estado inicial
-	SetGameCtx(GameCtx)
+    -- carrega o estado inicial manualmente para usar uma transição
+	GameState[GameCtx]:load()
 end
 
 function love.update(dt)
+    MainTransition:update(dt)
+    -- do not update the scene while transition is running
+    if MainTransition.isActive then return end
 	GameState[GameCtx]:update(dt)
 end
 
 function love.draw()
 	GameState[GameCtx]:draw()
+    MainTransition:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
